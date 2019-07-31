036bc006f7d9988cbb228efbd3abbb0c05f51843
Arjun Singh Mann <37778385+arjunmann73@users.noreply.github.com>
2019-07-31 22:25:44 +0400

    Update Fibonacci.c
    Added Naive and Top-Down implementations as well. (Removed copyright as well)

--- C/Fibonacci.c
+++ C/Fibonacci.c
@@ -3,12 +3,19 @@
 //  DynamicProgrammingEbook
 //
 //  Created by Arjun Singh on 07/29/19.
-//  Copyright © 2018 Solomon Kinard. All rights reserved.
 //
 
 #include <stdio.h>
 
-int fib(int n){
+// Naive recursive Fibonacci solution
+int naive(int n){
+    if(n < 2)
+        return n;
+    return naive(n-1) + naive(n-2);
+}
+
+// Bottom-up dynamic Fibonacci solution
+int bottomUp(int n){
     if(n<2)
         return n;
     int t1 = 1, t2 = 0, i;
@@ -20,10 +27,41 @@ int fib(int n){
     return t1 + t2;
 }
 
+int topDown1(int n, int cache[]){
+    if(cache[n]>=0)
+        return cache[n];
+
+    cache[n] = topDown(n-1, cache) + topDown(n-2, cache);
+    return cache[n];
+}
+
+// Top-down dynamic Fibonacci solution
+int topDown(int n){
+    if(n < 2)
+        return n;
+    int cache[n+1], i;
+    for(i = 0; i<=n ; i++)
+        cache[i] = -1;
+    // Fill initial values in cache
+    cache[0] = 0;
+    cache[1] = 1;
+    return topDown1(n, cache);
+}
+
+
 void test(){
     int i;
+    printf("Naive Method:\n");
     for (i=-2; i<9; i++) {
-      printf("i=%d, fib(i)=%d\n", i, fib(i));
+      printf("i=%d, naive(i)=%d\n", i, naive(i));
+    }
+    printf("\nTop-Down Method:\n");
+    for (i=-2; i<9; i++) {
+      printf("i=%d, topDown(i)=%d\n", i, topDown(i));
+    }
+    printf("\nBottom-Up Method:\n");
+    for (i=-2; i<9; i++) {
+      printf("i=%d, bottomUp(i)=%d\n", i, bottomUp(i));
     }
 }
 
